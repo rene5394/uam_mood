c7efda4435c7d41c5f3c4eb92f7662ef281d7d72
Rene Torres <rene_edgardo_2@hotmail.com>
2022-02-19 22:18:18 -0600

    Add repositories and services to the dependency container

--- src/container.ts
+++ src/container.ts
@@ -1,8 +1,17 @@
 import { Application } from 'express'
 import { asClass, createContainer } from 'awilix'
 import { scopePerRequest } from 'awilix-express'
+import { CommentService } from './services/comment.service'
+import { FeelingService } from './services/feeling.service'
+import { MoodService } from './services/mood.service'
+import { ReactionService } from './services/reaction.service'
+import { CommentMySQLRepository } from './services/repositories/implementation/mysql/comment.repository'
+import { CommentReactionMySQLRepository } from './services/repositories/implementation/mysql/commentReaction.repository'
+import { FeelingMySQLRepository } from './services/repositories/implementation/mysql/feeling.repository'
+import { MoodReactionMySQLRepository } from './services/repositories/implementation/mysql/moodReaction.repository'
+import { MoodMySQLRepository } from './services/repositories/implementation/mysql/mood.repository'
+import { ReactionMySQLRepository } from './services/repositories/implementation/mysql/reaction.repository'
 import { UserMySQLRepository } from './services/repositories/implementation/mysql/user.repository'
-import { UserService } from './services/user.service'
 
 export default (app: Application): void => {
     const container = createContainer({
@@ -12,9 +21,18 @@ export default (app: Application): void => {
     container.register({
         // Repositories
         userRepository: asClass(UserMySQLRepository).scoped(),
+        commentRepository: asClass(CommentMySQLRepository).scoped(),
+        commentReactionRepository: asClass(CommentReactionMySQLRepository).scoped(),
+        feelingRepository: asClass(FeelingMySQLRepository).scoped(),
+        moodRepository: asClass(MoodReactionMySQLRepository).scoped(),
+        moodReactionRepository: asClass(MoodMySQLRepository).scoped(),
+        reactionRepository: asClass(ReactionMySQLRepository).scoped(),
 
         // Services
-        userService: asClass(UserService).scoped()
+        commentService: asClass(CommentService).scoped(),
+        feelingService: asClass(FeelingService).scoped(),
+        moodService: asClass(MoodService).scoped(),
+        reactionService: asClass(ReactionService).scoped()
     })
 
     app.use(scopePerRequest(container))
