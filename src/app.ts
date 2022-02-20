d8efa84364b288a9e8e213d340892c4fab36f7ef
Rene Torres <rene_edgardo_2@hotmail.com>
2022-02-19 22:08:17 -0600

    Add JWT authentication

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,30 @@
-import express, { Application } from 'express'
-import { loadControllers } from 'awilix-express';
-import loadContainer from './container';
-import jwt from 'express-jwt';
-import cors from 'cors';
-import dotenv from 'dotenv'
+import { dotenv } from './config'
+import express, { Application, json } from 'express'
+import { loadControllers } from 'awilix-express'
+import loadContainer from './container'
+import jwt from 'express-jwt'
+import cors from 'cors'
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'development'
-process.env.APP_ENV = process.env.APP_ENV || 'development'
-
-dotenv.config({
-    path: `${__dirname}/../config/${process.env.APP_ENV}.env`
-})
+dotenv.config()
 
 const app: Application = express()
 
-app.use(express.json());
+app.use(json())
 
-app.use(cors());
+app.use(cors())
 
 loadContainer(app)
+
+if (process.env.jwt_secret_key) {
+    app.use(jwt({
+        secret: process.env.jwt_secret_key,
+        algorithms: ['HS256']
+    }))
+}
 
 app.use(loadControllers(
     'controllers/*.ts',
     { cwd: __dirname }
-));
+))
 
 export { app }
